3d07b622e94ce63038e3e739baf405c8a2996951
Ashot <ashmat98@gmail.com>
2019-09-25 13:10:14 +0400

    logarithm function with specific branch

---
+++ functions.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+pi = np.pi
+PI = np.pi
+
+def log(z, phi=-np.pi):
+    """
+    Logarithm with specific branch. Elementvise operation.
+    
+    Args:
+        z (complex): complex input to the function
+        phi (float, optional): Logarithm branch would be (phi..phi+2*pi).
+            Defaults to -np.pi.
+    
+    Returns:
+        complex: log(z)=ro*exp(it) t in range (phi..phi+2*pi)
+    """
+    delta = (phi+np.pi)
+    return np.log(z * np.exp(-1j*delta)) + 1j*delta

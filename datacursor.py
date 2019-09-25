0253d51fa31b4105ee2644fba692bb6f5be2c517
Ashot <ashmat98@gmail.com>
2019-09-25 13:11:34 +0400

    Add hover feature to color plot

---
+++ datacursor.py
@@ -0,0 +1,70 @@
+class HoverCursor(object):
+    """A simple data cursor widget that displays the x,y location of a
+    matplotlib artist when it is selected."""
+    def __init__(self, figure, offsets=(-20, 20), 
+                 formatter=lambda ax, x,y:'x: %0.2f\ny: %0.2f' %(x,y)):
+        """Create the data cursor and connect it to the relevant figure.
+        *figure* is the matplotlib figure. 
+        *offsets* is a tuple of (x,y) offsets in points from the selected
+            point to the displayed annotation box
+        *formatter* is the format string to be used. Note: For compatibility
+            with older versions of python, this uses the old-style (%) 
+            formatting specification. 
+        """
+        self.format = formatter
+        self.offsets = offsets  
+        self.figure = figure
+
+        self.annotations = {}
+        
+        self.figure.canvas.mpl_connect('motion_notify_event', self)
+    
+    def get_anotation(self, axis):
+        if axis not in self.annotations:
+            self.annotations[axis] = self.annotate(axis)
+        return self.annotations[axis]
+
+    def set_invisible(self):
+        for ax, anot in self.annotations.items():
+            anot.set_visible(False)
+
+    def annotate(self, ax, visible=False):
+        """Draws the annotation box for the given axis *ax*."""
+        annotation = ax.annotate("", xy=(0, 0), ha='right',
+                xytext=self.offsets, textcoords='offset points', va='bottom',
+                bbox=dict(boxstyle='round,pad=0.2', fc='yellow', alpha=0.5),
+                # arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0')
+                )
+        annotation.set_visible(visible)
+        return annotation
+
+    def __call__(self, event):
+        """Update and draw the annotation box for the pick event *event*."""
+        # Rather than trying to interpolate, just display the clicked coords
+        # This will only be called if it's within "tolerance", anyway.
+        if event.inaxes is None:
+            self.set_invisible() 
+            event.canvas.draw()
+            return
+        
+        ax = event.inaxes
+        x, y = event.xdata, event.ydata
+        
+        annotation = self.get_anotation(ax)
+        annotation.xy = x, y
+        annotation.set_text(self.format(ax, x, y))
+        self.set_invisible() 
+        annotation.set_visible(True)
+        event.canvas.draw()
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.subplot(2,1,1)
+    line1, = plt.plot(range(10), 'ro-')
+    plt.subplot(2,1,2)
+    line2, = plt.plot(range(10), 'bo-')
+
+    HoverCursor(plt.gcf(), offsets=(-10, 10))
+
+    plt.show()
--- plot_colors.py
+++ plot_colors.py
@@ -3,10 +3,11 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
 import numpy as np
+from datacursor import HoverCursor
 
 
 def plot(f, xlim, ylim, step, 
-    color_power=1, color_clip=None, figsize=(16, 8)):
+    color_power=1, color_clip=None, figsize=(16, 8), dpi=200, save_path=None):
     xlim = xlim[0], xlim[1]+1e-8
     ylim = ylim[0], ylim[1]+1e-8
     
@@ -31,25 +32,45 @@ def plot(f, xlim, ylim, step,
         # colors[:, :, :3] *= np.tanh(absW[:, :, np.newaxis] / np.max(absW)*2)
         return colors
     
-    def transform(x, lim, step, dtype="float"):
+    def transform(x, lim, step, dtype="int"):
         return ((-lim[0] + x) / step).astype(dtype)
+    def inverse_transform(x, lim, step, dtype="float"):
+        return (x * step + lim[0]).astype(dtype)
+    
     def label_pos(lim, step):
         labels = np.arange(lim[0], lim[1]+1e-8, 1)
         positions = transform(labels, lim, step, "int")
         return positions, labels
 
-
     
-    plt.figure(figsize=figsize)
-    plt.subplot(121)
+    
+    plt.figure(figsize=figsize, dpi=150)
+    ax_id = plt.subplot(121)
     plt.title("Input")
     plt.imshow(colors(Z.T, color_power), origin="lower")
     plt.xticks(*label_pos(xlim, step))
     plt.yticks(*label_pos(ylim, step))
     plt.grid(alpha=0.2)
-    plt.subplot(122)
-    plt.title("Output")    
+    
+    ax_f = plt.subplot(122)
+    plt.title("Output")   
     plt.imshow(colors(W.T, color_power), origin="lower")
     plt.xticks(*label_pos(xlim, step))
     plt.yticks(*label_pos(ylim, step))
     plt.grid(alpha=0.2)
+
+    def formatter(ax, x, y):
+        x = inverse_transform(x, xlim, step)
+        y = inverse_transform(y, ylim, step)
+        if ax == ax_id:
+            z = x + 1j*y
+        elif ax == ax_f:
+            z = f(x + 1j*y)
+        ro = np.abs(z)
+        phi = ((np.angle(z)) % (2*np.pi)) / np.pi
+        return 'x: %0.2f\ny: %0.2f\n$\\rho$: %0.2f\n$\\phi$: %0.2f$\\pi$' %(x, y, ro, phi)
+    
+    HoverCursor(plt.gcf(), formatter=formatter)
+    if save_path is not None:
+        plt.savefig(save_path, dpi=dpi)
+    return plt.gcf()
